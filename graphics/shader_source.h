2726aa5905e70092c450e01a001690509374aca0
Alexey Gerasev <alexey.gerasev@gmail.com>
2015-03-10 23:30:18 +0600

    newlines in shader

--- graphics/shader_source.h
+++ graphics/shader_source.h
@@ -10,7 +10,7 @@
 	(sizeof(array)/sizeof(array[0]))
 
 #ifdef __ANDROID__
-#define GLSL_PRECISION "precision mediump float;"
+#define GLSL_PRECISION "precision mediump float;\n"
 #else
 #define GLSL_PRECISION ""
 #endif
@@ -30,15 +30,15 @@ static const GLchar *ATTR_VERT[] = {A_COORD};
 static const GLchar *UNIF_VERT[] = {U_TRANSLATION, U_PROJECTION, U_MODELVIEW};
 static const GLchar *SRC_VERT = 
 	GLSL_PRECISION
-	"uniform vec2 "U_TRANSLATION";"
-	"uniform mat2 "U_PROJECTION";"
-	"uniform mat2 "U_MODELVIEW";"
-	"attribute vec2 "A_COORD";"
-	"varying vec2 "V_POSITION";"
-	"void main(void) {"
-		V_POSITION" = "A_COORD";"
-		"gl_Position = vec4("U_PROJECTION"*("U_MODELVIEW"*"A_COORD" + "U_TRANSLATION"),0.0,1.0);"
-	"}";
+	"uniform vec2 "U_TRANSLATION";\n"
+	"uniform mat2 "U_PROJECTION";\n"
+	"uniform mat2 "U_MODELVIEW";\n"
+	"attribute vec2 "A_COORD";\n"
+	"varying vec2 "V_POSITION";\n"
+	"void main(void) {\n"
+		V_POSITION" = "A_COORD";\n"
+		"gl_Position = vec4("U_PROJECTION"*("U_MODELVIEW"*"A_COORD" + "U_TRANSLATION"),0.0,1.0);\n"
+	"}\n";
 
 /* Uniforms */
 #define U_COLOR       "uColor"
@@ -46,54 +46,54 @@ static const GLchar *SRC_VERT =
 static const GLchar *UNIF_FRAG_FILL[] = {U_COLOR};
 static const GLchar *SRC_FRAG_FILL =
 	GLSL_PRECISION
-	"uniform vec4 "U_COLOR";"
-	"varying vec2 "V_POSITION";"
-	"void main(void) {"
-		"gl_FragColor = "U_COLOR";"
-	"}";
+	"uniform vec4 "U_COLOR";\n"
+	"varying vec2 "V_POSITION";\n"
+	"void main(void) {\n"
+		"gl_FragColor = "U_COLOR";\n"
+	"}\n";
 
 static const GLchar *UNIF_FRAG_QUAD[] = {U_COLOR,U_MODELVIEW};
 static const GLchar *SRC_FRAG_QUAD =
 	GLSL_PRECISION
-	"uniform vec4 "U_COLOR";"
-	"uniform mat2 "U_MODELVIEW";"
-	"varying vec2 "V_POSITION";"
-	"void main(void) {"
-		"vec2 lBounds = vec2(length("U_MODELVIEW"*vec2(1.0,0.0)),length("U_MODELVIEW"*vec2(0.0,1.0)));"
-		"gl_FragColor = "U_COLOR"*(clamp(0.5*min("
-		"  lBounds.x*min(1.0 - "V_POSITION".x, 1.0 + "V_POSITION".x),"
-		"  lBounds.y*min(1.0 - "V_POSITION".y, 1.0 + "V_POSITION".y)"
-		"),-0.5,0.5) + 0.5);"
-	"}";
+	"uniform vec4 "U_COLOR";\n"
+	"uniform mat2 "U_MODELVIEW";\n"
+	"varying vec2 "V_POSITION";\n"
+	"void main(void) {\n"
+		"vec2 lBounds = vec2(length("U_MODELVIEW"*vec2(1.0,0.0)),length("U_MODELVIEW"*vec2(0.0,1.0)));\n"
+		"gl_FragColor = "U_COLOR"*(clamp(0.5*min(\n"
+		"  lBounds.x*min(1.0 - "V_POSITION".x, 1.0 + "V_POSITION".x),\n"
+		"  lBounds.y*min(1.0 - "V_POSITION".y, 1.0 + "V_POSITION".y)\n"
+		"),-0.5,0.5) + 0.5);\n"
+	"}\n";
 
 static const GLchar *UNIF_FRAG_CIRCLE[] = {U_COLOR,U_MODELVIEW};
 static const GLchar *SRC_FRAG_CIRCLE =
 	GLSL_PRECISION
-	"uniform vec4 "U_COLOR";"
-	"uniform mat2 "U_MODELVIEW";"
-	"varying vec2 "V_POSITION";"
-	"void main(void) {"
-		"float lLen = length("V_POSITION");"
-		"vec2 lNorm = "V_POSITION"/lLen;"
-		"float lEllipseLen = length("U_MODELVIEW"*lNorm);"
-		"gl_FragColor = "U_COLOR"*(clamp(0.5*(1.0 - lLen)*lEllipseLen,-0.5,0.5) + 0.5);"
-	"}";
+	"uniform vec4 "U_COLOR";\n"
+	"uniform mat2 "U_MODELVIEW";\n"
+	"varying vec2 "V_POSITION";\n"
+	"void main(void) {\n"
+		"float lLen = length("V_POSITION");\n"
+		"vec2 lNorm = "V_POSITION"/lLen;\n"
+		"float lEllipseLen = length("U_MODELVIEW"*lNorm);\n"
+		"gl_FragColor = "U_COLOR"*(clamp(0.5*(1.0 - lLen)*lEllipseLen,-0.5,0.5) + 0.5);\n"
+	"}\n";
 
 #define U_INNER_MUL "uInnerMul"
 
 static const GLchar *UNIF_FRAG_RING[] = {U_COLOR,U_MODELVIEW,U_INNER_MUL};
 static const GLchar *SRC_FRAG_RING =
 	GLSL_PRECISION
-	"uniform vec4 "U_COLOR";"
-	"uniform mat2 "U_MODELVIEW";"
-  "uniform float "U_INNER_MUL";"
-	"varying vec2 "V_POSITION";"
-	"void main(void) {"
-		"float lLen = length("V_POSITION");"
-		"vec2 lNorm = "V_POSITION"/lLen;"
-		"float lEllipseLen = length("U_MODELVIEW"*lNorm);"
-		"gl_FragColor = "U_COLOR"*(clamp(min("
-      "0.5*(1.0 - lLen)*lEllipseLen,"
-      "0.5*(lLen - "U_INNER_MUL")*lEllipseLen"
-    "),-0.5,0.5) + 0.5);"
-	"}";
+	"uniform vec4 "U_COLOR";\n"
+	"uniform mat2 "U_MODELVIEW";\n"
+  "uniform float "U_INNER_MUL";\n"
+	"varying vec2 "V_POSITION";\n"
+	"void main(void) {\n"
+		"float lLen = length("V_POSITION");\n"
+		"vec2 lNorm = "V_POSITION"/lLen;\n"
+		"float lEllipseLen = length("U_MODELVIEW"*lNorm);\n"
+		"gl_FragColor = "U_COLOR"*(clamp(min(\n"
+      "0.5*(1.0 - lLen)*lEllipseLen,\n"
+      "0.5*(lLen - "U_INNER_MUL")*lEllipseLen\n"
+    "),-0.5,0.5) + 0.5);\n"
+	"}\n";
